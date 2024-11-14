8d0c020aee06a5282b62a11d038f89ef07bf33ce
Khan Atik Faisal <khanatik1176@gmail.com>
2024-11-14 20:49:21 +0600

    basics-added

---
+++ 6sense-assignment/src/app/page.js
@@ -0,0 +1,15 @@
+'use client';
+import Image from "next/image";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div className=" bg-white grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-sans">
+        <div className="heading-area">
+              <h1 className="text-4xl text-black">Admin Dashboard</h1>
+        </div>
+        
+    </div>
+  );
+}
